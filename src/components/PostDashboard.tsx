9b0239a1ae5cd7c0c356c2993afe569bead57d31
Sangjin Kim <rla2892@naver.com>
2024-09-23 11:36:21 +0900

    Convert post dashboard into server component to make use of cache of data fetch

--- src/components/PostDashboard.tsx
+++ src/components/PostDashboard.tsx
@@ -1,38 +1,18 @@
-"use client"
-
-import { useEffect, useState } from 'react'
 import PostList from './PostList'
-import { PostType, PostsResponse } from '../types'
+import { PostsResponse } from '../types'
+import { BASE_URL } from '@/config'
 
-export default function PostDashboard() {
-    const [posts, setPosts] = useState<PostType[]>([])
-    const [loading, setLoading] = useState<boolean>(true)
-    const [error, setError] = useState<string | null>(null)
-
-    const fetchPosts = async () => {
-        try {
-            const res = await fetch(`/api/posts`)
-            if (!res.ok) {
-                throw new Error(`게시글을 불러오는 데 실패했습니다.`)
-            }
-            const data: PostsResponse = await res.json()
-            // 최신 게시글이 위에 오도록 정렬
-            const sortedPosts = data.posts.sort((a, b) => new Date(b.createdAt).getTime() - new Date(a.createdAt).getTime())
-            setPosts(sortedPosts)
-        } catch (err: any) {
-            setError(err.message)
-        } finally {
-            setLoading(false)
+export default async function PostDashboard() {
+    try {
+        const res = await fetch(`${BASE_URL}/api/posts`)
+        if (!res.ok) {
+            throw new Error(`게시글을 불러오는 데 실패했습니다.`)
         }
+        const data: PostsResponse = await res.json()
+        // 최신 게시글이 위에 오도록 정렬
+        const sortedPosts = data.posts.sort((a, b) => new Date(b.createdAt).getTime() - new Date(a.createdAt).getTime())
+        return <PostList posts={sortedPosts} />
+    } catch (err: any) {
+        return <p className="text-red-500">{err.message}</p>
     }
-
-    useEffect(() => {
-        fetchPosts()
-    }, [])
-
-    return <>
-        {loading && <p>로딩 중...</p>}
-        {error && <p className="text-red-500">{error}</p>}
-        {!loading && !error && <PostList posts={posts} />}
-    </>
 }
--- src/config/index.ts
+++ src/config/index.ts
@@ -1,3 +1,2 @@
-const DB_API_URL = process.env.NEXT_PUBLIC_DB_API_URL || `http://localhost:4000`
-
-export default DB_API_URL
+export const DB_API_URL = process.env.NEXT_PUBLIC_DB_API_URL || `http://localhost:4000`
+export const BASE_URL = process.env.NEXT_PUBLIC_BASE_URL || `http://localhost:3000`
