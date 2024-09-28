f903333821b0ac25bb6b991bf9af8b50d5eac95a
Sangjin Kim <rla2892@naver.com>
2024-09-28 11:24:00 +0900

    Implement API for registering new user

---
+++ src/app/api/register/route.ts
@@ -0,0 +1,55 @@
+import { UserType } from "@/types"
+import { NextResponse } from 'next/server'
+import { getAllUsers, registerUser } from '@/lib/db'
+
+export async function POST(
+    request: Request
+) {
+    try {
+        const { id, name, email, password }: UserType = await request.json()
+
+        // 기본적인 유효성 검사
+        if (!id || !name || !email || !password) {
+            return NextResponse.json(
+                { message: `필수 필드가 누락되었습니다.` },
+                { status: 400 }
+            )
+        }
+
+        // 이미 등록된 이메일인지 확인
+        const existingUsers = await getAllUsers()
+        const isExistingUser = existingUsers.some(user => user.email === email)
+
+        if (isExistingUser) {
+            return NextResponse.json(
+                { message: `이미 등록된 이메일입니다.` },
+                { status: 400 }
+            )
+        }
+
+        // 등록
+        const newUser = {
+            id,
+            name,
+            email,
+            password,
+        }
+        try {
+            const response = await registerUser(newUser)
+            return NextResponse.json(
+                response,
+                { status: 201 }
+            )
+        } catch (error) {
+            return NextResponse.json(
+                { message: `사용자 생성에 실패했습니다. ${error}` },
+                { status: 500 }
+            )
+        }
+    } catch (error) {
+        return NextResponse.json(
+            { message: `요청을 처리하는 데 실패했습니다. ${error}` },
+            { status: 500 }
+        )
+    }
+}
--- src/lib/db.ts
+++ src/lib/db.ts
@@ -9,3 +9,18 @@ export async function getAllUsers(): Promise<UserType[]> {
     const data: UserType[] = await res.json()
     return data
 }
+
+export async function registerUser(user: UserType): Promise<UserType> {
+    const res = await fetch(`${DB_API_URL}/users`, {
+        method: `POST`,
+        headers: {
+            'Content-Type': `application/json`,
+        },
+        body: JSON.stringify(user),
+    })
+    if (!res.ok) {
+        throw new Error(`사용자 생성에 실패했습니다.`)
+    }
+    const data: UserType = await res.json()
+    return data
+}
