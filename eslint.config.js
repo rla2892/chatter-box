3d793e811e6c1d4a49d512225288ab0a297800f9
Sangjin Kim <rla2892@naver.com>
2024-09-29 09:04:56 +0900

    Change eslint rule for quotes

--- eslint.config.js
+++ eslint.config.js
@@ -16,21 +16,21 @@ module.exports = [
             parser: typescriptParser, // Use TypeScript parser
         },
         rules: {
-            'indent': [`error`, 4], // 4 spaces
-            'linebreak-style': [`error`, `unix`], // Unix linebreaks
-            'quotes': [`error`, `backtick`], // Backticks
-            'semi': [`error`, `never`], // No semicol
-            'no-var': `error`, // No var
-            'prefer-const': `error`, // Prefer const
-            'spaced-comment': [`error`, `always`], // Always space comments
-            'no-undef': `error`, // No undefined
-            'eqeqeq': `error`, // Use ===
-            'no-console': process.env.NODE_ENV === `production` ? `error` : `warn`, // No console in production
-            'no-debugger': process.env.NODE_ENV === `production` ? `error` : `warn`, // No debugger in production
+            "indent": [`error`, 4], // 4 spaces
+            "linebreak-style": [`error`, `unix`], // Unix linebreaks
+            "quotes": [`error`, `double`, { "allowTemplateLiterals": true }], // Backticks
+            "semi": [`error`, `never`], // No semicol
+            "no-var": `error`, // No var
+            "prefer-const": `error`, // Prefer const
+            "spaced-comment": [`error`, `always`], // Always space comments
+            "no-undef": `error`, // No undefined
+            "eqeqeq": `error`, // Use ===
+            "no-console": process.env.NODE_ENV === `production` ? `error` : `warn`, // No console in production
+            "no-debugger": process.env.NODE_ENV === `production` ? `error` : `warn`, // No debugger in production
             // 'no-alert': process.env.NODE_ENV === `production` ? `error` : `warn`, // No alert in production
-            'no-unused-vars': process.env.NODE_ENV === `production` ? `error` : `warn`, // No unused vars in production
-            'no-constant-condition': process.env.NODE_ENV === `production` ? `error` : `warn`, // No constant conditions in production
-            'no-empty': process.env.NODE_ENV === `production` ? `error` : `warn`, // No empty blocks in production
+            "no-unused-vars": process.env.NODE_ENV === `production` ? `error` : `warn`, // No unused vars in production
+            "no-constant-condition": process.env.NODE_ENV === `production` ? `error` : `warn`, // No constant conditions in production
+            "no-empty": process.env.NODE_ENV === `production` ? `error` : `warn`, // No empty blocks in production
         },
     },
 ]
