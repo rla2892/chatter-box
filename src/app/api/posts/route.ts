adffe03967b4bd03b9b69d8adf0b36089544862e
Sangjin Kim <rla2892@naver.com>
2024-09-30 11:34:23 +0900

    Remove meaningless comments

--- src/app/api/posts/route.ts
+++ src/app/api/posts/route.ts
@@ -19,7 +19,6 @@ export async function GET() {
 export async function POST(
     request: Request
 ) {
-    // 기존 데이터 가져오기
     const sessionObj = await getServerSession(authOptions)
     const userId = sessionObj?.user?.id ?? ``
     if (!userId) {
